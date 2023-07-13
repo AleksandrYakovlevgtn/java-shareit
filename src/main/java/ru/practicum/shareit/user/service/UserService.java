f9f16896366f8b9661b081aed9ae2abe147a4fe8
Aleksandr Yakovlev <yak0vlev.as@yandex.ru>
2023-07-13 17:14:44 +0300

    ТЗ13: Внес правки ревью.

--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -4,7 +4,6 @@ import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.Collection;
-import java.util.List;
 
 public interface UserService {
     UserDto add(User user);
