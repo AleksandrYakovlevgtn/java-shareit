1cfadb4e9bbb72d79d61dc5115e589308c84a192
AleksandrYakovlevgtn <120303197+AleksandrYakovlevgtn@users.noreply.github.com>
2023-08-26 23:03:22 +0300

    ТЗ16: add-docker исправления ревью

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
@@ -11,10 +11,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import ru.practicum.shareit.booking.valid.StartBeforeEndDateValid;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@StartBeforeEndDateValid
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class BookItemRequestDto {
     long itemId;
